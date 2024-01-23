d68fa99dc9573893c2acc846271a77f60def2268
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-01-23 01:15:47 -0500

    Add Bootstrap, login and register page to the routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import socketIO from "socket.io-client";
-const socket = socketIO.connect("http://localhost:4000");
+import Login from './components/Login';
+import Register from './components/Register';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import socket from './components/redux_components/actions/authActions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login socket={socket} />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
