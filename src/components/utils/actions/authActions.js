c0691f57e955fa85ec88a66493ee5fb2ed2c7b50
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2024-02-19 03:15:29 -0500

    Fix Actions issues
    `registerUser` and `loginUser` now delivers Actions correctly, instead of delivering `UNDEFINED`.
    User register and login function works correctly.

--- src/components/utils/actions/authActions.js
+++ src/components/utils/actions/authActions.js
@@ -1,4 +1,5 @@
 import socketIO from "socket.io-client";
+import { createAction } from "@reduxjs/toolkit";
 
 const socket = socketIO.connect("http://localhost:4000");
 
@@ -9,35 +10,40 @@ const socket = socketIO.connect("http://localhost:4000");
  * @returns {(function(*): void)|*}
  */
 const registerUser = (userData, navigate) => {
-    // Emit the register event to the server with the user data.
-    socket.emit("register", userData);
+    return dispatch => {
+        // Emit the register event to the server with the user data.
+        socket.emit("register", userData);
 
-    // Listen for the newRegisteredUser event from the server.
-    socket.on("newRegisteredUser", data => {
-        // If the registration was successful, redirect the user to the login page.
-        data.status === "00000" ? navigate("/login") : console.log(data.message);
-    });
+        // Listen for the newRegisteredUser event from the server.
+        socket.on("newRegisteredUser", data => {
+            // If the registration was successful, redirect the user to the login page.
+            data.status === "00000" ? navigate("/login") : console.log(data.message);
+        });
+    }
 };
 
 /**
  * Login a user.
  * @param userData
- * @param dispatch
+ * @param navigate
  * @returns {(function(*): void)|*}
  */
-const loginUser = (userData, dispatch) => {
-    // Emit the login event to the server with the user data.
-    socket.emit("login", userData);
+const loginUser = (userData, navigate) => {
+    return dispatch => {
+        // Emit the login event to the server with the user data.
+        socket.emit("login", userData);
 
-    socket.on("loggedInUser", (data) => {
-        if (data.status === "00000") {
-            const { token } = data;
-            localStorage.setItem("jwtToken", token);
-            dispatch(setCurrentUser(token));
-        } else {
-            console.log(data.message);
-        }
-    });
+        socket.on("loggedInUser", (data) => {
+            if (data.status === "00000") {
+                const { token } = data;
+                localStorage.setItem("jwtToken", token);
+                dispatch(setCurrentUser(token));
+                navigate("/channels/@me");
+            } else {
+                console.log(data.message);
+            }
+        });
+    }
 };
 
 /**
@@ -45,12 +51,11 @@ const loginUser = (userData, dispatch) => {
  * @param token
  * @returns {{payload, type: string}}
  */
-const setCurrentUser = token => {
+const setCurrentUser = createAction("SET_CURRENT_USER", token => {
     return {
-        type: "SET_CURRENT_USER",
         payload: token
-    };
-};
+    }
+});
 
 /**
  * Logout a user.
